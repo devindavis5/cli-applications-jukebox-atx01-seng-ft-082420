dc5ef89aef7e1fb9cc9147dd8f3b598f38685f59
Devin Davis <devinrdavis555@gmail.com>
2020-08-18 16:12:31 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,35 @@
-# Add your code here
+require 'pry'
+
+def help
+  puts 'I accept the following commands:'
+  puts '- help : displays this help message'
+  puts '- list : displays a list of songs you can play'
+  puts '- play : lets you choose a song to play'
+  puts '- exit : exits this program'
+end
+
+def list(songs_array)
+  
+songs_array.each_with_index {|item, index| puts "#{index + 1}. #{item}"}
+
+end
+
+def play(songs_array)
+  
+ puts "Please enter a song name or number:"
+ 
+ songs_array.each | 
+ 
+ 
+ 
+   puts "Invalid input, please try again"
+ end
+  
+end
+
+def exit_jukebox
+puts 'Goodbye'
+end
+
+def run
+end
